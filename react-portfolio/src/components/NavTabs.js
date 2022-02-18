736c78383642c9ceefb2c7baf16f5c44393c5b48
Awad Sharif <awadsharif9@gmail.com>
2022-02-18 01:05:25 -0800

    updated portfolio assigment

--- react-portfolio/src/components/NavTabs.js
+++ react-portfolio/src/components/NavTabs.js
@@ -4,7 +4,9 @@ import './styles/NavTabs.css';
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
+      
     <ul className="nav nav-tabs bg-gradient-to-r from-black to-gray-500 ... p-5">
+        <p>Awad Sharif</p>
       <li className="nav-item">
         <a
           href="#home"
--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -6,7 +6,44 @@ export default function Portfolio() {
   return (
     <div className="portfolio bg-desert">
         <div className="portfolio-card">
-            <a href="https://github.com/asharif123/rental-finder"><img src="/images/rental-finder.JPG"></img>https://rental-finders.herokuapp.com/</a>
+            <div className="portfolio-projects-card">
+                <div>
+                    <h1>Rental Finders</h1>
+                    <a href="https://github.com/asharif123/rental-finder"><img className="portfolio-card-image" src="/images/rental-finder.JPG" alt="Rental Finder"></img></a>
+                    <a className="deployed-link" href="https://rental-finders.herokuapp.com/">Deployed Link</a>
+                </div>
+                <div>
+                    <h1>Tech Blog</h1>
+                    <a href="https://github.com/asharif123/tech-blog"><img className="portfolio-card-image" src="/images/tech-blog.JPG" alt="Rental Finder"></img></a>
+                    <a className="deployed-link" href="https://still-garden-56258.herokuapp.com/">Deployed Link</a>
+                </div>
+                <div>
+                    <h1>Rental Finders</h1>
+                    <a href="https://github.com/asharif123/rental-finder"><img className="portfolio-card-image" src="/images/rental-finder.JPG" alt="Rental Finder"></img></a>
+                    <a className="deployed-link" href="https://rental-finders.herokuapp.com/">Deployed Link</a>
+                </div>
+                <div>
+                    <h1>Rental Finders</h1>
+                    <a href="https://github.com/asharif123/rental-finder"><img className="portfolio-card-image" src="/images/rental-finder.JPG" alt="Rental Finder"></img></a>
+                    <a className="deployed-link" href="https://rental-finders.herokuapp.com/">Deployed Link</a>
+                </div>
+                <div>
+                    <h1>Rental Finders</h1>
+                    <a href="https://github.com/asharif123/rental-finder"><img className="portfolio-card-image" src="/images/rental-finder.JPG" alt="Rental Finder"></img></a>
+                    <a className="deployed-link" href="https://rental-finders.herokuapp.com/">Deployed Link</a>
+                </div>
+                <div>
+                    <h1>Rental Finders</h1>
+                    <a href="https://github.com/asharif123/rental-finder"><img className="portfolio-card-image" src="/images/rental-finder.JPG" alt="Rental Finder"></img></a>
+                    <a className="deployed-link" href="https://rental-finders.herokuapp.com/">Deployed Link</a>
+                </div>
+
+
+
+
+
+
+            </div>
         </div>
       <footer className="footer-icons">
           <a href="https://www.linkedin.com/in/awad-sharif/"><img src="/images/linkedIn.JPG" alt="LinkedIn"></img></a>
