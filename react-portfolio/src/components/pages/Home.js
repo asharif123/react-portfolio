6ccfa744d970197a39aedec2de8b8e1f11aad8aa
Awad Sharif <awadsharif9@gmail.com>
2022-02-18 22:53:50 -0800

    added responsiveness to website

--- react-portfolio/src/components/pages/Home.js
+++ react-portfolio/src/components/pages/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="homePage bg-desert">
       <p className="home-summary">My name is Awad Sharif!</p>
-      <p className="home-summary-information">I am an aspiring web developer with over 4 years of quality control/quality assurance experience.</p>      
+      <p className="home-summary-information">I am an aspiring full stack web developer.</p>      
       <footer className="footer-icons">
           <a href="https://www.linkedin.com/in/awad-sharif/"><img src={linkedIn}alt="LinkedIn"></img></a>
           <a href="https://github.com/asharif123/"><img src={gitHub} alt="Github"></img></a>
